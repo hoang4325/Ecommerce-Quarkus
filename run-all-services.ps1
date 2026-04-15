c1177b6ba4ef9908c6d0a626b9a758acf57de7a8
Hoang4325 <hoangheir@gmail.com>
2026-04-15 22:41:38 +0700

    refactor: restructure project into backend, frontend, and tools directories

--- run-all-services.ps1
+++ run-all-services.ps1
@@ -22,14 +22,14 @@ Write-Host "Starting backed services in separate cmd windows..." -ForegroundColo
 foreach ($service in $services) {
     Write-Host "Starting $service..."
     # Mở 1 cửa sổ cmd mới, chuyển vào thư mục service và chạy mvn quarkus:dev
-    Start-Process "cmd.exe" -ArgumentList "/c title $service & cd .\$service & mvn quarkus:dev"
+    Start-Process "cmd.exe" -ArgumentList "/c title $service & cd .\backend\$service & mvn quarkus:dev"
 }
 
 Write-Host "Waiting 40 seconds for backend services to boot before starting API Gateway..." -ForegroundColor Magenta
 Start-Sleep -Seconds 40
 
 Write-Host "Starting api-gateway..." -ForegroundColor Green
-Start-Process "cmd.exe" -ArgumentList "/c title api-gateway & cd .\api-gateway & mvn quarkus:dev"
+Start-Process "cmd.exe" -ArgumentList "/c title api-gateway & cd .\backend\api-gateway & mvn quarkus:dev"
 
 Write-Host "All services and API Gateway are starting up!" -ForegroundColor Cyan
 Write-Host "Make sure to run 'docker-compose up -d' for Postgres, Keycloak, and Kafka if you haven't already." -ForegroundColor Yellow
